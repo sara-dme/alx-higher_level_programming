c0b75af8067a210a5cda35d99fa1ed58081624a9
sara-dme <s.daoumy@gmail.com>
2023-08-08 10:50:50 -0700

    6 and 7 tasks

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i < 10:
-        print("0{}, ".format(i), end="")
-    elif i == 99:
+    if i == 99:
         print("{}\n".format(i), end="")
     else:
-        print("{}, ".format(i), end="")
+        print("{:02d}, ".format(i), end="")
