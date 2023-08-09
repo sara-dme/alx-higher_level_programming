5dd66bd4650b577367c1c68401d701824482b821
sara-dme <s.daoumy@gmail.com>
2023-08-09 12:54:36 -0700

    fix

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for n in range(122, 60, -1):
-    if n % 2 == 0:
-        i = 0
-    else:
+for n in range(122, 96, -1):
+    i = 0
+    if n % 2 != 0:
         i = 32
-    print("{}".format(chr(n - diff)), end="")
+    print('{}'.format(chr(n - i)), end="")
