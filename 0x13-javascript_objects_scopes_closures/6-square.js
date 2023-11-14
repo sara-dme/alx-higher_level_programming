0affc93138038b6414f23d7c9ac1eb4474abb305
sara-dme <s.daoumy@gmail.com>
2023-11-14 14:57:02 -0800

    standard style

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 
-module.exports = class Square extends require('./5-squre') {
-	charPoint (c) {
+module.exports = class Square extends require('./5-square') {
+	charPrint (c) {
 		if (c === undefined) this.print();
 		else {
 			for (let i = 0; i < this.height; i++) {
--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -2,9 +2,10 @@
 
 
 exports.nbOccurences = function (list, searchElement) {
-	let nbr = 0;
-	for (let i = 0; i < list.length; i++) {
-		if (searchElement === list[i]) nbr++;
-	}
-	return nbr;
-};
+  let nbr = 0
+  for (let i = 0; i < list.length; i++) {
+    if (searchElement === list[i]) nbr++
+  }
+  return nbr;
+}
+
