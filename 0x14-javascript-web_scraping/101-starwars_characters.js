c4aa87870fd5a21a48631a149358399394bbc36f
sara-dme <s.daoumy@gmail.com>
2024-01-03 07:17:16 -0800

    task 8

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+const filmId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${filmId}`;
+
+request(url, (error, response, body) => {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+      for (const charUrl of characters) {
+	request(charUrl, (error, response, body) => {
+	  if (!error) {
+		  console.log(JSON.parse(body).name);
+	  }
+	});
+      }
+  }
+});
