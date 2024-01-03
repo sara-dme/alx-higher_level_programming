6f29ac2775edc29e0419b059fafedb12536cc914
sara-dme <s.daoumy@gmail.com>
2024-01-03 00:29:05 -0800

    task 3-4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+let count = 0;
+
+request.get(url, (error, response, body) => {
+	if (!error) {
+		const data = JSON.parse(body);
+		data.results.forEach((film) => {
+			film.characters.forEach((character) => {
+				if (character.includes('18')) {
+					count += 1;
+				}
+			});
+		});
+		console.log(count);
+	}
+});
