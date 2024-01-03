64a627d8e917066aef8f31e9440b6c78713868bc
sara-dme <s.daoumy@gmail.com>
2024-01-03 00:43:13 -0800

    task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = process.argv[2];
+const data = {};
+
+request(url, (error, response, body) => {
+	if (!error) {
+		const todos = JSON.parse(body);
+		for (const task of todos) {
+			if (task.completed && !(task.userId in data)) {
+				data[task.userId] = 0;
+			}
+			if (task.completed) {
+				data[task.userId] += 1;
+			}
+		}
+		console.log(data);
+	}
+});
